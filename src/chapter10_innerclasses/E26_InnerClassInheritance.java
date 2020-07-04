dbfe9d422f570c45d50a2bde9216c983f664b718
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2020-07-04 15:40:55 +0200

    Advance through chapter 10 (inner classes) // task 26

---
+++ src/chapter10_innerclasses/E26_InnerClassInheritance.java
@@ -0,0 +1,25 @@
+package chapter10_innerclasses;
+
+class E26_OuterOne {
+    class InnerOne {
+        InnerOne(String s) {
+            System.out.println(s);
+        }
+    }
+}
+
+class E26_OuterTwo {
+    class InnerTwo extends E26_OuterOne.InnerOne {
+        InnerTwo(E26_OuterOne o, String s) {
+            o.super(s);
+        }
+    }
+}
+
+public class E26_InnerClassInheritance {
+    public static void main(String[] args) {
+        E26_OuterOne o1 = new E26_OuterOne();
+        E26_OuterTwo o2 = new E26_OuterTwo();
+        E26_OuterTwo.InnerTwo i2 = o2.new InnerTwo(o1, "hi");
+    }
+}
