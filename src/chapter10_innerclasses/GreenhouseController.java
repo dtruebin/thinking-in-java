0a127f256d53319e1f4f999ec24804a4c9a01945
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2020-07-04 13:17:06 +0200

    Add GreenhouseControls code from chapter 10 (inner classes)

---
+++ src/chapter10_innerclasses/GreenhouseController.java
@@ -0,0 +1,29 @@
+package chapter10_innerclasses;
+
+import chapter10_innerclasses.controller.Event;
+
+public class GreenhouseController {
+    public static void main(String[] args) {
+        GreenhouseControls gc = new GreenhouseControls();
+
+        // Instead of hard-wiring, you could parse
+        // configuration information from a text file here:
+        gc.addEvent(gc.new Bell(900));
+        Event[] eventList = {
+                gc.new ThermostatNight(0),
+                gc.new LightOn(200),
+                gc.new LightOff(400),
+                gc.new ThermostatDay(1400)
+        };
+        gc.addEvent(gc.new Restart(2000, eventList));
+
+        if (args.length == 1)
+            gc.addEvent(
+                    gc.new Terminate(
+                            new Integer(args[0])
+                    )
+            );
+
+        gc.run();
+    }
+}
---
+++ src/chapter10_innerclasses/GreenhouseControls.java
@@ -0,0 +1,138 @@
+package chapter10_innerclasses;
+
+import chapter10_innerclasses.controller.Controller;
+import chapter10_innerclasses.controller.Event;
+
+public class GreenhouseControls extends Controller {
+    private boolean light = false;
+    private String thermostat = "Day";
+
+    public class LightOn extends Event {
+        public LightOn(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            light = true;
+        }
+
+        @Override
+        public String toString() {
+            return "Light is on";
+        }
+    }
+
+    public class LightOff extends Event {
+        public LightOff(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            light = false;
+        }
+
+        @Override
+        public String toString() {
+            return "Light is off";
+        }
+    }
+
+    public class ThermostatNight extends Event {
+        public ThermostatNight(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            thermostat = "Night";
+        }
+
+        @Override
+        public String toString() {
+            return "Thermostat on night setting";
+        }
+    }
+
+    public class ThermostatDay extends Event {
+        public ThermostatDay(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            thermostat = "Day";
+        }
+
+        @Override
+        public String toString() {
+            return "Thermostat on day setting";
+        }
+    }
+
+    // An example of an action() that inserts a
+    // new one of itself into the event list:
+    public class Bell extends Event {
+
+        public Bell(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            addEvent(new Bell(delayTime));
+        }
+
+        @Override
+        public String toString() {
+            return "Bing!";
+        }
+    }
+
+    public class Restart extends Event {
+        private final Event[] eventList;
+
+        public Restart(long delayTime, Event[] eventList) {
+            super(delayTime);
+            this.eventList = eventList;
+            for (Event e :
+                    eventList) {
+                addEvent(e);
+            }
+        }
+
+        @Override
+        public void action() {
+            for (Event e :
+                    eventList) {
+                e.start(); // Rerun each event
+                addEvent(e);
+            }
+            start(); // Rerun this Event
+            addEvent(this);
+        }
+
+        @Override
+        public String toString() {
+            return "Restarting system";
+        }
+    }
+
+    public class Terminate extends Event {
+        public Terminate(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            System.exit(0);
+        }
+
+        @Override
+        public String toString() {
+            return "Terminating";
+        }
+    }
+
+}
