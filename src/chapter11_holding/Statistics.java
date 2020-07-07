30f5472aafbaa300228b19368dfcbd037b59616c
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2020-07-07 22:21:26 +0200

    Advance through chapter 11 (holding your objects) // add example of Map application

---
+++ src/chapter11_holding/Statistics.java
@@ -0,0 +1,20 @@
+package chapter11_holding;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Statistics {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < 10_000; i++) {
+            int n = rand.nextInt(20);
+            Integer freq = map.get(n);
+            map.put(n, freq == null ? 1 : freq + 1);
+        }
+
+        System.out.println(map);
+    }
+}
