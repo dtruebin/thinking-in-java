869643bc3df64f39435606733b26cf0c0220301d
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2020-07-27 16:25:23 +0200

    Advance through chapter 11 (holding your objects) // task 29
    Create a simple class that inherits from Object and contains no members,
    and show that you cannot successfully add multiple elements of that
    class to a PriorityQueue. This issue will be fully explained in the
    Containers in Depth chapter.

---
+++ src/main/java/chapter11_holding/E29_PriorityQueue.java
@@ -0,0 +1,29 @@
+/*
+Create a simple class that inherits from Object and contains no members, and
+show that you cannot successfully add multiple elements of that class to a
+PriorityQueue. This issue will be fully explained in the Containers in Depth
+chapter.
+ */
+
+package main.java.chapter11_holding;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Issue {
+}
+
+public class E29_PriorityQueue {
+    public static void main(String[] args) {
+        Queue<Issue> pq = new PriorityQueue<>();
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Adding item #" + i);
+            try {
+                pq.offer(new Issue());
+                System.out.println("Added!");
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
+}
