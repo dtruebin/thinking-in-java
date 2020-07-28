af14c7f03e5e1b2bc49ba8e7d7078c2242e45479
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2020-07-28 16:58:57 +0200

    Advance through chapter 12 (exceptions) // task 7
    Use logger in the catch clause

--- src/main/java/chapter12_exceptions/E07_Logging.java
+++ src/main/java/chapter12_exceptions/E07_Logging.java
@@ -4,15 +4,27 @@ Modify Exercise 3 so that the catch clause logs the results.
 
 package main.java.chapter12_exceptions;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
 @SuppressWarnings({"MismatchedReadAndWriteOfArray", "ConstantConditions"})
 public class E07_Logging {
+    static Logger logger = Logger.getLogger("E07_Logging");
+
+    static void logException(Exception e) {
+        StringWriter stringWriter = new StringWriter();
+        e.printStackTrace(new PrintWriter(stringWriter));
+        logger.severe(stringWriter.toString());
+    }
+
     public static void main(String[] args) {
         int[] array = new int[5];
 
         try {
             System.out.println(array[10]);
         } catch (Exception e) {
-            e.printStackTrace(System.err);
+            logException(e);
         }
     }
 }
