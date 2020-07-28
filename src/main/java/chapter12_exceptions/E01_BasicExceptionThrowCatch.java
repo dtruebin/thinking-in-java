7a659fc22cbfef5511c63e4c2bdf37612fc6a072
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2020-07-28 13:02:40 +0200

    Advance through chapter 12 (exceptions) // task 1
    Create a class with a main( ) that throws an object of class Exception
    inside a try block. Give the constructor for Exception a String
    argument. Catch the exception inside a catch clause and print the String
    argument. Add a finally clause and print a message to prove you were
    there.

---
+++ src/main/java/chapter12_exceptions/E01_BasicExceptionThrowCatch.java
@@ -0,0 +1,20 @@
+/*
+Create a class with a main( ) that throws an object of class Exception inside a
+try block. Give the constructor for Exception a String argument. Catch the
+exception inside a catch clause and print the String argument. Add a finally
+clause and print a message to prove you were there.
+ */
+
+package main.java.chapter12_exceptions;
+
+public class E01_BasicExceptionThrowCatch {
+    public static void main(String[] args) {
+        try {
+            throw new Exception("Oops");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } finally {
+            System.out.println("Inside the `finally` clause");
+        }
+    }
+}
