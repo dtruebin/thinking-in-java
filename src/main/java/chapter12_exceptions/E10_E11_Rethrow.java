3c0783eac32652309c8d35aef210f12b619a8e3c
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2020-07-29 10:21:02 +0200

    Advance through chapter 12 (exceptions) // task 11
    Repeat the previous exercise, but inside the catch clause, wrap g( )’s
    exception in a RuntimeException.

--- src/main/java/chapter12_exceptions/E10_E11_Rethrow.java
+++ src/main/java/chapter12_exceptions/E10_E11_Rethrow.java
@@ -1,16 +1,18 @@
 /*
+Exercise 10:
 Create a class with two methods, f( ) and g( ). In g( ), throw an exception of a
 new type that you define. In f( ), call g( ), catch its exception and, in the
 catch clause, throw a different exception (of a second type that you define).
 Test your code in main( ).
+
+Exercise 11:
+Repeat the previous exercise, but inside the catch clause, wrap g( )’s exception
+in a RuntimeException.
  */
 
 package main.java.chapter12_exceptions;
 
 class CustomException1 extends Exception {
-}
-
-class CustomException2 extends Exception {
 }
 
 class Thrower {
@@ -19,17 +21,17 @@ class Thrower {
         throw new CustomException1();
     }
 
-    static void f() throws CustomException2 {
+    static void f() {
         System.out.println("Start of f()");
         try {
             g();
         } catch (Exception e) {
-            throw new CustomException2();
+            throw new RuntimeException(e);
         }
     }
 }
 
-public class E10_Rethrow {
+public class E10_E11_Rethrow {
     public static void main(String[] args) {
         try {
             Thrower.g();
