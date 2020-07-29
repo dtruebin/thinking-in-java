775d702e5ae19c495561a3fe9a28f99d1166a9a9
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2020-07-29 19:11:37 +0200

    Advance through chapter 12 (exceptions) // task 27
    Modify Exercise 3 to convert the exception to a RuntimeException.

--- src/main/java/chapter12_exceptions/E03_E27_ArrayIndexOutOfBoundsException.java
+++ src/main/java/chapter12_exceptions/E03_E27_ArrayIndexOutOfBoundsException.java
@@ -1,18 +1,22 @@
 /*
+Exercise 3:
 Write code to generate and catch an ArraylndexOutOfBoundsException.
+
+Exercise 27:
+Modify Exercise 3 to convert the exception to a RuntimeException.
  */
 
 package main.java.chapter12_exceptions;
 
 @SuppressWarnings({"MismatchedReadAndWriteOfArray", "ConstantConditions"})
-public class E03_ArrayIndexOutOfBoundsException {
+public class E03_E27_ArrayIndexOutOfBoundsException {
     public static void main(String[] args) {
         int[] array = new int[5];
 
         try {
             System.out.println(array[10]);
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new RuntimeException(e);
         }
     }
 }
