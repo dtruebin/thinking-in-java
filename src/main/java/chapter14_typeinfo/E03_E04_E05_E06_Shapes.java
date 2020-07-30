299f6cb82fea42b7cbcab85618fc544b111535f5
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2020-07-30 18:36:39 +0200

    Advance through chapter 14 (RTTI) // task 6
    Modify Shapes.java so that it can "highlight" (set a flag in) all shapes
    of a particular type. The toString( ) method for each derived Shape
    should indicate whether that Shape is "highlighted."

--- src/main/java/chapter14_typeinfo/E03_E04_E05_E06_Shapes.java
+++ src/main/java/chapter14_typeinfo/E03_E04_E05_E06_Shapes.java
@@ -11,59 +11,80 @@ performing the downcast.
 Exercise 5:
 Implement a rotate(Shape) method in Shapes.java, such that it checks to see if
 it is rotating a Circle (and, if so, doesn't perform the operation).
+
+Exercise 6:
+Modify Shapes.java so that it can "highlight" (set a flag in) all shapes of a
+particular type. The toString( ) method for each derived Shape should indicate
+whether that Shape is "highlighted."
  */
 
 package main.java.chapter14_typeinfo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 abstract class Shape {
+    private static final List<Shape> shapes = new ArrayList<>();
+    private boolean isHighlighted;
+
+    Shape() {
+        shapes.add(this);
+    }
+
+    static void highlightAll(Class<?> c) {
+        for (Shape shape : shapes) {
+            if (shape.getClass().equals(c)) {
+                shape.highlight();
+            } else {
+                shape.unhighlight();
+            }
+        }
+    }
+
     void draw() {
-        System.out.println(this + ".draw()");
+        System.out.println(this + " draw()");
+    }
+
+    void highlight() {
+        isHighlighted = true;
+    }
+
+    void unhighlight() {
+        isHighlighted = false;
     }
 
     void rotate(int deg) {
         System.out.println("Rotating " + this + " by " + deg + "°");
     }
 
-    abstract public String toString();
+    public String toString() {
+        return this.getClass().getSimpleName() + (isHighlighted ? " (highlighted)" : "");
+    }
 }
 
 class Circle extends Shape {
     void rotate(int deg) {
         throw new UnsupportedOperationException("Can't rotate a circle");
     }
-
-    public String toString() {
-        return "Circle";
-    }
 }
 
 class Rhomboid extends Shape {
-    public String toString() {
-        return "Rhomboid";
-    }
 }
 
 class Square extends Shape {
-    public String toString() {
-        return "Square";
-    }
 }
 
 class Triangle extends Shape {
-    public String toString() {
-        return "Triangle";
-    }
 }
 
 @SuppressWarnings("ConstantConditions")
-public class E03_E04_E05_Shapes {
+public class E03_E04_E05_E06_Shapes {
     public static void main(String[] args) {
         List<Shape> shapeList = Arrays.asList(
                 new Circle(), new Square(), new Triangle()
         );
+        Shape.highlightAll(Square.class);
         for (Shape shape : shapeList) {
             shape.draw();
             if (!(shape instanceof Circle)) {
@@ -72,6 +93,7 @@ public class E03_E04_E05_Shapes {
         }
         Shape s = new Rhomboid();
         Rhomboid r = (Rhomboid) s;
+        Shape.highlightAll(Rhomboid.class);
         r.draw();
         if (s instanceof Circle) {
             Circle c = (Circle) s;
