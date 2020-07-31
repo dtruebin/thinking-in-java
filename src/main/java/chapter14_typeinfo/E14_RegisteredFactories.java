42cbc78fe6c144aebc26fcc7e3ff561aea7c7c3b
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2020-07-31 17:08:29 +0200

    Advance through chapter 14 (RTTI) // task 14
    A constructor is a kind of factory method. Modify RegisteredFactories.java
    so that instead of using an explicit factory,
    the class object is stored in the List, and newInstance( ) is used to
    create each object.

---
+++ src/main/java/chapter14_typeinfo/E14_RegisteredFactories.java
@@ -0,0 +1,87 @@
+/*
+A constructor is a kind of factory method. Modify RegisteredFactories.java so
+that instead of using an explicit factory, the class object is stored in the
+List, and newInstance( ) is used to create each object.
+ */
+
+package main.java.chapter14_typeinfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+class Part {
+    private static final Random rand = new Random(47);
+    // Registering Class Factories in the base class.
+    static List<Class<? extends Part>> partClasses = new ArrayList<>();
+
+    static {
+        partClasses.add(FuelFilter.class);
+        partClasses.add(AirFilter.class);
+        partClasses.add(CabinAirFilter.class);
+        partClasses.add(OilFilter.class);
+        partClasses.add(FanBelt.class);
+        partClasses.add(PowerSteeringBelt.class);
+        partClasses.add(GeneratorBelt.class);
+    }
+
+    public static Part createRandom() {
+        Part part;
+        int n = rand.nextInt(partClasses.size());
+        try {
+            part = partClasses.get(n).newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return part;
+    }
+
+    public String toString() {
+        return getClass().getSimpleName();
+    }
+}
+
+class Filter extends Part {
+}
+
+class FuelFilter extends Filter {
+}
+
+class AirFilter extends Filter {
+}
+
+class CabinAirFilter extends Filter {
+}
+
+class OilFilter extends Filter {
+}
+
+class Belt extends Part {
+}
+
+class FanBelt extends Belt {
+}
+
+class GeneratorBelt extends Belt {
+}
+
+class PowerSteeringBelt extends Belt {
+}
+
+public class E14_RegisteredFactories {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            System.out.println(Part.createRandom());
+    }
+} /* Output:
+GeneratorBelt
+CabinAirFilter
+GeneratorBelt
+AirFilter
+PowerSteeringBelt
+CabinAirFilter
+FuelFilter
+PowerSteeringBelt
+PowerSteeringBelt
+FuelFilter
+*///:~
