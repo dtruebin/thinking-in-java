5792a3f1beffbde20b00c585ba205a4e0f6572d4
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2020-07-31 14:00:58 +0200

    Advance through chapter 14 (RTTI) // task 9
    Modify the previous exercise so that it uses Class.getDeclaredFields( )
    to also display information about the fields in a class.

--- src/main/java/chapter14_typeinfo/E08_09_ClassHierarchyAndFields.java
+++ src/main/java/chapter14_typeinfo/E08_09_ClassHierarchyAndFields.java
@@ -1,23 +1,41 @@
 /*
+Exercise 8:
 Write a method that takes an object and recursively prints all the classes in
 that object’s hierarchy.
+
+Exercise 9:
+Modify the previous exercise so that it uses Class.getDeclaredFields( ) to also
+display information about the fields in a class.
  */
 
 package main.java.chapter14_typeinfo;
 
+import java.lang.reflect.Field;
+
 class Top {
+    protected int topField;
 }
 
 class Middle extends Top {
+    private final String middleField = "Yo";
+    Side s;
+
+    static class Side {
+    }
 }
 
 class Bottom extends Middle {
+    static long bottomField1;
+    double bottomField2 = topField;
 }
 
 
-public class E08_Hierarchy {
+public class E08_09_ClassHierarchyAndFields {
     static void printClassHierarchy(Class<?> c) {
         System.out.println(c.getName());
+        for (Field field : c.getDeclaredFields()) {
+            System.out.println("- " + field);
+        }
         if (c.getSuperclass() != null) {
             printClassHierarchy(c.getSuperclass());
         }
