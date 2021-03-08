aa9551501319364e3d56dc26e1d7e7df1ba59b5c
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2021-03-08 02:06:05 +0100

    Improve test strength for MyLinkedList

--- src/test/java/datastructures/MyLinkedListTest.java
+++ src/test/java/datastructures/MyLinkedListTest.java
@@ -60,6 +60,7 @@ class MyLinkedListTest {
         assertTrue(list.contains(null));
     }
 
+    @SuppressWarnings("RedundantCollectionOperation")
     @Test
     void containsAll() {
         List<Integer> c = new LinkedList<>(Arrays.asList(-100, 2, null, 3));
@@ -69,6 +70,7 @@ class MyLinkedListTest {
         list.add(-100);
 
         assertTrue(list.containsAll(c));
+        assertFalse(list.containsAll(Collections.singleton(100500)));
     }
 
     @Test
@@ -111,7 +113,12 @@ class MyLinkedListTest {
     }
 
     @Test
-    void removeSingleNodeByIndexMakesListEmpty() {
+    void removeNonExistentElementReturnsFalse() {
+        assertFalse(list.remove(Integer.valueOf(1)));
+    }
+
+    @Test
+    void removeSingleElementByIndexMakesListEmpty() {
         list.add(5);
         assertFalse(list.isEmpty());
         list.remove(0);
@@ -119,8 +126,16 @@ class MyLinkedListTest {
     }
 
     @Test
+    void removeByIndexReturnsRemovedElement() {
+        final int number = 123;
+        list.add(number);
+        assertEquals(number, list.remove(0));
+    }
+
+    @Test
     void removeAll() {
         list.addAll(Arrays.asList(1, 2, 2, null, 3, 4, null));
+        assertFalse(list.removeAll(Collections.singleton(5)));
         assertTrue(list.removeAll(Arrays.asList(1, 2, 4, null)));
         assertEquals(list, Collections.singletonList(3));
     }
@@ -176,6 +191,11 @@ class MyLinkedListTest {
     }
 
     @Test
+    void addReturnsTrue() {
+        assertTrue(list.add(1));
+    }
+
+    @Test
     void addToEmptyListAtPositionZero() {
         list.add(0, null);
     }
@@ -203,6 +223,69 @@ class MyLinkedListTest {
     }
 
     @Test
+    void listIteratorHasPrevious() {
+        list.add(0);
+        ListIterator<Integer> listIterator = list.listIterator();
+        assertFalse(listIterator.hasPrevious());
+        listIterator.next();
+        assertTrue(listIterator.hasPrevious());
+    }
+
+    @Test
+    void listIteratorNextReturnsElement() {
+        list.addAll(Arrays.asList(2, 4, 6));
+        ListIterator<Integer> listIterator = list.listIterator();
+        for (Integer element : list) {
+            assertEquals(element, listIterator.next());
+        }
+    }
+
+    @Test
+    void listIteratorPreviousReturnsElement() {
+        list.addAll(Arrays.asList(2, 4, 6));
+        ListIterator<Integer> listIterator = list.listIterator(list.size());
+        for (int i = list.size() - 1; i >= 0; i--) {
+            assertEquals(list.get(i), listIterator.previous());
+        }
+    }
+
+    @Test
+    void listIteratorNoNextOrPreviousThrowsException() {
+        ListIterator<Integer> listIterator = list.listIterator();
+        assertThrows(NoSuchElementException.class, listIterator::previous);
+        assertThrows(NoSuchElementException.class, listIterator::next);
+    }
+
+    @Test
+    void listIteratorPreviousAndNextIndex() {
+        list.addAll(Arrays.asList(2, 4, 6));
+        ListIterator<Integer> listIterator = list.listIterator();
+        for (int i = 0; i < list.size(); i++) {
+            assertEquals(i - 1, listIterator.previousIndex());
+            assertEquals(i, listIterator.nextIndex());
+            listIterator.next();
+        }
+    }
+
+    @Test
+    void listIteratorPreviousReducesNextIndex() {
+        list.addAll(Arrays.asList(2, 4, 6));
+        ListIterator<Integer> listIterator = list.listIterator(list.size());
+        for (int i = list.size(); i > 0; i--) {
+            int oldNext = listIterator.nextIndex();
+            listIterator.previous();
+            int newNext = listIterator.nextIndex();
+            assertEquals(oldNext - 1, newNext);
+        }
+    }
+
+    @Test
+    void listIteratorSetWithNoMoveThrowsException() {
+        ListIterator<Integer> listIterator = list.listIterator();
+        assertThrows(IllegalStateException.class, () -> listIterator.set(5));
+    }
+
+    @Test
     void getFirstAndLastIndexByElement() {
         list.add(1);
         list.add(null);
@@ -223,8 +306,23 @@ class MyLinkedListTest {
 
     @Test
     void addAllForEmptyCollectionReturnsFalse() {
-        Set<Integer> c = new HashSet<>();
-        assertFalse(list.addAll(c));
+        assertFalse(list.addAll(Collections.emptySet()));
+        assertFalse(list.addAll(0, Collections.emptySet()));
+    }
+
+    @Test
+    void addAllSuccessfulReturnsTrue() {
+        assertTrue(list.addAll(Collections.singleton(1)));
+        assertTrue(list.addAll(0, Collections.singleton(1)));
+    }
+
+    @Test
+    void addAllAtBadIndexThrowsException() {
+        assertAll(
+                () -> assertThrows(IndexOutOfBoundsException.class, () -> list.addAll(-1, Collections.emptySet())),
+                () -> assertDoesNotThrow(() -> list.addAll(0, Collections.emptySet())),
+                () -> assertThrows(IndexOutOfBoundsException.class, () -> list.addAll(1, Collections.emptySet()))
+        );
     }
 
     @SuppressWarnings("UseBulkOperation")
